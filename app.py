fc668d03f800ac8ce4cb6b2396cda21a6f291a9e
Jacopopsen <58552183+Jacopopsen@users.noreply.github.com>
2022-11-11 09:47:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    while(true):
-        print("Test")
-    return 'Hello, fucker 2.0!'
+x = 0
+while True:
+    x =+ 1
+    print(x)
